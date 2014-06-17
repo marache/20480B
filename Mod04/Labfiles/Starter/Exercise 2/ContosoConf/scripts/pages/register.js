3c7dae1af506a98f06babcf95b1e32b1fb97b5df
Mathieu MARACHE <mathieu.marache@cstb.fr>
2014-06-17 15:08:19 +0200

    Mod04 Exercice2 finished

--- Mod04/Labfiles/Starter/Exercise 2/ContosoConf/scripts/pages/register.js
+++ Mod04/Labfiles/Starter/Exercise 2/ContosoConf/scripts/pages/register.js
@@ -3,19 +3,28 @@ var form = document.getElementById("registration-form");
 var submitButton = form.querySelector("button");
     
 // TODO: Task 1 - Get the password <input> elements from the DOM by ID
-// var passwordInput = ...;
-// var confirmPasswordInput = ...;
+var passwordInput = document.getElementById("password");
+var confirmPasswordInput = document.getElementById("confirm-password");
 
 var checkPasswords = function () {
     // TODO: Task 2 - Compare passwordInput value to confirmPasswordInput value
+    var ok = passwordInput.value === confirmPasswordInput.value
 
     // TODO: Task 3 - If passwords don't match then display error message on confirmPasswordInput (using setCustomValidity)
     //                If passwords do match then clear the error message (setCustomValidity with empty string)
+    if (!ok) {
+        confirmPasswordInput.setCustomValidity("Passwords don't match, please enter the same password !");
+    }
+    else {
+        confirmPasswordInput.setCustomValidity("");
+    }
 };
 
 var addPasswordInputEventListeners = function () {
     // TODO: Task 4 - Listen for the "input" event on passwordInput and confirmPasswordInput.
     //       Call the checkPasswords function
+    passwordInput.addEventListener("input", checkPasswords, false); 
+    confirmPasswordInput.addEventListener("input", checkPasswords, false);
 };
 
 var formSubmissionAttempted = function() {
